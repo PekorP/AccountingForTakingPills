947d32b2c17becfc03c8ae48b8aca3973ef711a9
Pekor <pashachistjakov@gmail.com>
2021-09-15 19:14:41 +0300

    Create and add project in dir

---
+++ MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountingForTakingPills
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+          
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            System.Diagnostics.Process.Start($"https://apteka.ru/search/?q={textBox1.Text}&page=1");
+            else
+            {
+                MessageBox.Show("Test");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start($"https://apteka.ru");
+        }
+    }
+}
